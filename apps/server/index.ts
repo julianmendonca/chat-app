f81c15539ee36788d2e4b9852c16b5d942015f28
julian mendonca <julian@capicua.com.uy>
2022-07-07 01:34:09 -0300

    first commit

---
+++ apps/server/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+const PORT = 3001;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(
+  cors({
+    origin: ["http://192.168.0.71:3000", "http://localhost:3000"],
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Well done!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+io.on("connection", function (socket) {
+  console.log("Connected succesfully to the socket ...");
+  socket.join("chat");
+  socket.on("send message", function (data) {
+    socket.to("chat").emit("send message", data);
+    socket.emit("send message", data);
+  });
+});
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from "react";
 import { Button } from "ui";
+import { io } from "socket.io-client";
 
+const socket = io("http://192.168.0.71:3001", { transports: ["websocket"] });
 export default function Web() {
+  const [chat, setChat] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    socket.on("send message", (data) => {
+      setChat((chat) => [...chat, data]);
+    });
+  }, []);
+
+  const sendMessage = () => {
+    if (!inputValue) return;
+    socket.emit("send message", inputValue);
+    setInputValue("");
+  };
+
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>The amazing chat!</h1>
+      <div
+        style={{
+          height: "50vh",
+          overflowY: "scroll",
+          marginBottom: 30,
+          border: "1px solid black",
+          maxWidth: "50vw",
+        }}
+      >
+        {chat.map((message, index) => (
+          <p key={message + index}>{message}</p>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            sendMessage();
+          }
+        }}
+      />
+      <button onClick={sendMessage}>Send message</button>
     </div>
   );
 }
